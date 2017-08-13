e9af46d11225d7de7a9f9eb44022ba29eafa5dc5
yasharma <yashsharma205@gmail.com>
2017-08-13 15:20:31 +0530

    Add react test

---
+++ src/__tests__/App.test.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+import App from '../components/App';
+
+it('renders without crashing', () => {
+	const div = document.createElement('div');
+	ReactDom.render(<App />, div);
+});
