ea04883efb03b72ecd82c40945dff3e177bfd3e4
yasharma <yashsharma205@gmail.com>
2017-09-23 13:14:11 +0530

    Update production port to 8080

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,7 +5,8 @@ module.exports = {
         script: "./server.js",
         watch: false,
         env: {
-          "NODE_ENV": "development",
+          "PORT": 8080,
+          "NODE_ENV": "production",
         }
       }
   ]
