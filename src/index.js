5df200e4cabc130b5085e9a208fd9c31011b7333
yasharma <yashsharma205@gmail.com>
2018-02-15 21:58:30 +0530

    Repush last updated code

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import { Provider } from 'react-redux';
 import App from './components/App';
 import {store, history} from './store';
 
+
 // Vendor CSS
 import "react-spinner/react-spinner.css";
 import 'font-awesome/css/font-awesome.min.css';
